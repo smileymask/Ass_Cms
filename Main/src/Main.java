ef46b9d7780823986706ffd56549f33e865ed62c
Maskly1508 <62088873+smileymask@users.noreply.github.com>
2020-11-13 10:36:54 +0700

    update

---
+++ Main/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        int n=input.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=input.nextInt();
+        }
+        int max=a[0];
+        for(int i=0;i<n;i++){
+            if(max < a[i]){
+                max=a[i];
+            }
+        }
+        System.out.print(max);
+
+    }
+}
